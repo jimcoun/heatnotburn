8b05d0d1d5ceab66df5e7e3e9612c29a5f39d655
Στρτης (ΕΠ) Κουνάς Δημήτριος <d.kounas@army.gr>
2022-04-06 15:38:22 +0300

    Modal continue

--- _includes/age-verification/age-verification.js
+++ _includes/age-verification/age-verification.js
@@ -11,7 +11,7 @@
 	function getCookie(cname) {
 		var name = cname + "=";
 		var ca = document.cookie.split(';');
-		
+
 		for (var i=0; i < ca.length; i++) {
 			var c = ca[i].trim();
 			if (c.indexOf(name) === 0) return c.substring(name.length,c.length);
@@ -20,33 +20,48 @@
 	}
 
 	function checkCookie() {
-		var age = getCookie("ageVerify");
-			
-		if (age === "over21") {
-			// Check cookie: if exists and age passes, allow user into site
-			setCookie("ageVerify", "over21", 1);				
-		} else {
-			// If cookie value is under 21, or if cookie does not exist, create modal window and ask user to respond
-			$('body').prepend("<div class=\"age-check\"><div class=\"text-wrapper\"><p>Are you 21 years or older?</p><button class=\"confirm\">Yes</button><button class=\"cancel\">No</button></div></div>");
 
-			// Get rid of modal once button is clicked
+    $(".root").hide();
+		var age = getCookie("ageVerify");
+
+		if (age === "over18") {
+			// Check cookie: if exists and age passes, allow user into site
+			// setCookie("ageVerify", "over18", 10);
+      $("#age-verification-modal").hide();
+      $(".root").show();
+		} else if (age === "under18") {
+      actionsUnder18();
+      $("#age-verification-modal").show();
+    } else {
+			// If cookie value is under 21, or if cookie does not exist, create modal window and ask user to respond
+			//$('body').prepend("<div class=\"age-check\"><div class=\"text-wrapper\"><p>Are you 21 years or older?</p><button class=\"confirm\">Yes</button><button class=\"cancel\">No</button></div></div>");
+
+      // Get rid of modal once button is clicked
 			$('.age-check button').on('click', function() {
 				$('.age-check').hide();
 			});
 
-			$('button.confirm').on('click', function() {
+      $("#age-verification-modal").show();
+
+			$('#button-over-18').on('click', function() {
 				// If user responds yes, change age value
-				age = "over21";
-				setCookie("ageVerify", "over21", 1);
+				age = "over18";
+				setCookie("ageVerify", "over18", 10);
+        $(".root").show();
+        $("#age-verification-modal").hide();
 			});
 
-			$('button.cancel').on('click', function() {
-				// If user clicks cancel, direct user to /under-21 page
-				window.location.replace("http://www.leefoil.com/under-21");
-				setCookie("ageVerify", "under21", 1);
+			$('#button-under-18').on('click', function() {
+				setCookie("ageVerify", "under18", 10);
+        actionsUnder18();
 			});
 		}
 	}
+
+  function actionsUnder18() {
+    $('#button-over-18').hide();
+    $('#button-under-18').hide();
+  }
 
 	$(function() {
 		checkCookie();
