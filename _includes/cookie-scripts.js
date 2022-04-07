fe36c7bc3cdf1556620273fffe9fc9b7c1ff417e
Στρτης (ΕΠ) Κουνάς Δημήτριος <d.kounas@army.gr>
2022-04-07 14:59:40 +0300

    Glo post and other changes

---
+++ _includes/cookie-scripts.js
@@ -0,0 +1,86 @@
+(function () {
+  var SOURCES = window.TEXT_VARIABLES.sources;
+  window.Lazyload.js(SOURCES.jquery, function () {
+    function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function getCookie(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(';');
+
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+      }
+      return "";
+    }
+
+    function checkAgeCookie() {
+
+      var age = getCookie("ageVerify");
+
+      if (age === "over18") {
+        // Check cookie: if exists and age passes, allow user into site
+        // setCookie("ageVerify", "over18", 10);
+        $("#age-verification-modal").hide();
+        $(".root").show();
+      } else if (age === "under18") {
+        actionsUnder18();
+        $("#age-verification-modal").show();
+      } else {
+        // If cookie value is under 21, or if cookie does not exist, create modal window and ask user to respond
+        //$('body').prepend("<div class=\"age-check\"><div class=\"text-wrapper\"><p>Are you 21 years or older?</p><button class=\"confirm\">Yes</button><button class=\"cancel\">No</button></div></div>");
+
+        // Get rid of modal once button is clicked
+        $('.age-check button').on('click', function () {
+          $('.age-check').hide();
+        });
+
+        $("#age-verification-modal").show();
+
+        $('#button-over-18').on('click', function () {
+          // If user responds yes, change age value
+          age = "over18";
+          setCookie("ageVerify", "over18", 10);
+          $(".root").show();
+          $("#age-verification-modal").hide();
+        });
+
+        $('#button-under-18').on('click', function () {
+          //setCookie("ageVerify", "under18", 10);
+          actionsUnder18();
+        });
+      }
+    }
+
+    function checkNotificationCookie() {
+
+      var acceptedCookie = getCookie("acceptedCookies");
+
+      if (acceptedCookie !== "yes") {
+        $(".cookie-notification").show();
+
+        $('#button-cookies').on('click', function () {
+          setCookie("acceptedCookies", "yes", 30);
+          $(".cookie-notification").hide();
+        });
+
+      }
+    }
+
+    function actionsUnder18() {
+      $('#modal-text').text('Δεν είναι δυνατή η πρόσβασή σου στην ιστοσελίδα.');
+      $('#button-over-18').hide();
+      $('#button-under-18').hide();
+    }
+
+    $(function () {
+      checkAgeCookie();
+      checkNotificationCookie();
+    });
+  });
+})();
